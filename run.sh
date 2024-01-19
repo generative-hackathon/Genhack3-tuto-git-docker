5f1fd08a402b7fcb6875d5914eacc52d175c4c1d
michael-allouche <mjs.allouche@gmail.com>
2024-01-19 15:33:46 +0100

    add  all files

---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+##############################################
+# DO NOT MODIFY THIS FILE
+##############################################
+
+# Script for running your simulation
+##############################################
+
+run(){
+    # pull the repo !
+    git pull origin master
+    echo "Repo pulled !"  
+
+    echo "Running the model ..." 
+    docker image rm -f genhack3  # delete the existing image
+    docker build -t genhack3 .  # build new image
+    docker run --name container-sim -v ${PWD}/data:/data genhack3 # run the container and mount the data as volume
+    # copy output files to host
+    docker cp container-sim:/check.log ${PWD}/check.log
+    docker cp container-sim:/output.npy ${PWD}/output.npy
+    # remove the contaier
+    docker rm container-sim
+    echo "... end of run" 
+    
+    git add check.log
+    git commit -m "New submission"
+    git push origin master
+    echo "Pushed to the repo" 
+}
+
+run
